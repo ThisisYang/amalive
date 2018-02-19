2975da8400072fa5f648b383af39f217f047fbc5
yang <y.alex.sheng@gmail.com>
2018-02-19 13:14:43 -0500

    revisit and update. Will implement test

--- run.go
+++ run.go
@@ -2,104 +2,64 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"strconv"
-	"strings"
-	"os"
-	//"io"
 	"io/ioutil"
-	//"time"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 )
 
-
-var endpoints TCPflag
-var Interval = flag.Int("interval", 2, "interval in seconds between each retry. if retry is not set, interval will be ignored")
-var Retry = flag.Int("retry", 3, "times to retry")
+var ip = flag.String("ip", "127.0.0.1", "IP to connect")
+var port = flag.Int("port", 22, "port to check")
+var cinterval = flag.Int("cintv", 5, "check interval in seconds between tcp check")
+var rinterval = flag.Int("rintv", 2, "retry interval in seconds between each retry. if retry is not set, interval will be ignored")
+var retry = flag.Int("retry", 3, "times to retry")
 var debug = flag.Bool("debug", false, "debug mode")
-var Timeout = flag.Int("timeout", 5, "I/O timeout in seconds for both each connection and read for the first byte")
-// --tcp=ip:port
-// --udp=ip:port
-// --ping=ip
-
-type TCPflag struct {
-	Endpoint []TCP
-}
-
-type TCP struct {
-	IP   string
-	Port int
-}
-
-//converts struct as a string
-func (endpoint *TCPflag) String() string {
-	return fmt.Sprint(endpoint.Endpoint)
-}
-
-// Set called by flag.Parse function. Initializes the flag
-func (endpoint *TCPflag) Set(value string) error {
-	// --tcp ip:port,ip:port,ip2:port2
-	for _, item := range strings.Split(value, ",") {
-		arr := strings.Split(item, ":")
-		ip := arr[0]
-		port, err := strconv.Atoi(arr[1])
-		if err != nil {
-			return err
-		}
-		instance := TCP{IP: ip, Port: port}
-		//instance := TCP{IP: arr[0], PORT: strconv.Atoi(arr[1])}
-		endpoint.Endpoint = append(endpoint.Endpoint, instance)
-	}
-	return nil
-}
-
-func init() {
-	flag.Var(&endpoints, "tcp", "list of tcp endpoint, for example 127.0.0.1:22. if multiple endpoint, use ',' to seperate")
-}
+var timeout = flag.Int("timeout", 1, "I/O timeout in seconds for both each connection and read for the first byte")
 
 func main() {
 	flag.Parse()
+	ci := NewCheckInfo(*ip, *port, *timeout, *rinterval, *retry)
 
 	if *debug {
-		SetUpLogger(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
-		Debug.Println("Debug mode is turned on")
+		setUpLogger(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
+		Debug.Println("Debug mode")
 	} else {
-		SetUpLogger(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
+		setUpLogger(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
 	}
-	Debug.Printf("retry: %v times, Interval between each retry; %vs, Timeout: %vs", *Retry, *Interval, *Timeout)
+	Debug.Printf("check ednpiont: %v, retry: %v times, Interval between each retry; %vs, Timeout: %vs", ci.endpoint, ci.retry, ci.retryIntv, ci.timeout)
+
+	done := make(chan struct{})
+	var wg sync.WaitGroup
+	go waitForSignal(done)
 
 	//resp, in millisecond
-	var resp float64
-	for _, endpoint := range endpoints.Endpoint {
-		//fmt.Println(endpoint.IP, endpoint.Port)
-		resp = TCPCheck(endpoint.IP, endpoint.Port)
-	}
-	fmt.Println(resp)
+	wg.Add(1)
+	go func() {
+		t := time.NewTicker(time.Duration(*cinterval) * time.Second)
+		defer t.Stop()
+		defer wg.Done()
+		for {
+			select {
+			case <-done:
+				Info.Println("received termination signal, stopping all goroutines")
+				return
+			case <-t.C:
+				ci.tcpCheck()
+			}
+		}
+	}()
 
-	//fmt.Println(endpoints.Endpoint[1].IP, endpoints.Endpoint[1].Port)
-	//fmt.Println(*Interval, *Retry, *Timeout)
-	//ip := endpoints.Endpoint[0].IP
-	//c := endpoints.Endpoint[0].Port
-	//resp := 0
-	//count := 0
-	//for (resp == 0) && (count < 3) {
-	//	resp = TCPCheckOnce(ip, c)
-	//	count += 1
-	//}
+	wg.Wait()
+
 }
 
-/*
-// CommandOptions are options client provided
-type CommandOptions struct {
-	Addr     string
-	Port     int
-	Protocol string
-	Interval time.Duration
+// waitForSignal will close done channel when received signal
+func waitForSignal(done chan struct{}) {
+	sigs := make(chan os.Signal, 1)
+	// register sigs channel to receive SIGINT and SIGTERM signal
+	signal.Notify(sigs, os.Interrupt, os.Kill, syscall.SIGTERM) //syscall.SIGINT, syscall.SIGTERM
+	<-sigs
+	close(done)
 }
-
-// InstallFlags are function initalize the flag value
-func (commandOpts *CommandOptions) InstallFlags() {
-	flag.StringVar(&commandOpts.Addr, "IP", "127.0.0.1", "remote IP address")
-	flag.IntVar(&commandOpts.Port, "Port", 22, "remote port")
-	flag.StringVar(&commandOpts.Protocol, "TCP", "tcp", "Protocol to use")
-}
-*/
--- tcp.go
+++ tcp.go
@@ -1,113 +1,57 @@
 package main
 
 import (
-	//"bufio"
 	"fmt"
-	"io"
-	//"io/ioutil"
-	//"log"
+
 	"net"
-	//"os"
 	"time"
-	//"unicode/utf8"
 )
 
-func TCPCheck(IP string, Port int) float64 {
-	endpoint := fmt.Sprintf("%s:%v", IP, Port)
-	var totalSecond time.Duration
-	for i := 0; i<*Retry; i++{
-		if i != 0 {
-			time.Sleep(time.Duration(*Interval)*time.Second)
-		}
-		Debug.Printf("try: %v", i)
-		one := make([]byte, 1)
+type checkInfo struct {
+	endpoint  string
+	retry     int
+	retryIntv int
+	timeout   int
+}
+
+// NewCheckInfo return a checkInfo struct
+func NewCheckInfo(ip string, port, timeout, retry, retryIntv int) *checkInfo {
+	endpoint := fmt.Sprintf("%s:%v", ip, port)
+	if (retry == 0) && (retryIntv > 0) {
+		panic("retry interval is set but retry is 0")
+	}
+	return &checkInfo{
+		endpoint:  endpoint,
+		retry:     retry,
+		retryIntv: retryIntv,
+		timeout:   timeout,
+	}
+}
+
+func (c *checkInfo) tcpCheck() {
+	var totalSec time.Duration
+	for i := 1; i <= c.retry+1; i++ {
 		t := time.Now()
-		conn, err := net.DialTimeout("tcp", endpoint, time.Duration(*Timeout)*time.Second)
-		if err != nil {
-			Debug.Println("no connection", err)
-			totalSecond += time.Duration(*Timeout)*time.Second
-			continue
+		succ := checkOnce(c.endpoint, c.timeout)
+		totalSec += time.Now().Sub(t)
+		if succ == true {
+			avg := totalSec.Seconds() * 1000 / float64(i)
+			fmt.Println(avg)
+			Info.Printf("Success on %vst try, Avg connection time: %f ms\n", i, avg)
+			return
 		}
-		t1 := time.Now()
-		Debug.Printf("%s:%v connected", IP, Port)		
-		Debug.Printf("connection time: %s", time.Now().Sub(t))
-		conn.SetReadDeadline(time.Now().Add(time.Duration(*Timeout) * time.Second))
-		num, err := conn.Read(one)
-		if err == io.EOF {
-			Debug.Println("EOF", num)
-			totalSecond += time.Duration(*Timeout)*time.Second
-			continue
-		}
-		totalSecond += time.Now().Sub(t)
-		Debug.Printf("Read time: %v", time.Now().Sub(t1))
-		Debug.Printf("Summary: try: %v, time took: %v, totalSecond: %s", i, time.Now().Sub(t), totalSecond)
-		conn.Close()
-
 	}
-	return (totalSecond.Seconds())*1000/float64(*Retry)
+	Error.Printf("Failed to connect all %v times. Dail timeout: %v seconds\n", c.retry+1, c.timeout)
 }
 
-
-func TCPCheckOnce(IP string, Port int) int {
-	endpoint := fmt.Sprintf("%s:%v", IP, Port)
-
-	one := make([]byte, 1)
-	conn, err := net.DialTimeout("tcp", endpoint, 1*time.Second)
-	Debug.Println("here")
-	if err == nil {
-		Debug.Println("connected")
-		defer conn.Close()
-		conn.SetReadDeadline(time.Now().Add(1 * time.Second))
-		num, err := conn.Read(one)
-		if err == io.EOF {
-			Debug.Println("no read")
-			return 0
-		}
-		Debug.Println("read")
-		return num
-	} else {
+// checkOnce will try dial with timeout. return true if connected, otherwise false
+func checkOnce(endpoint string, timeout int) bool {
+	conn, err := net.DialTimeout("tcp", endpoint, time.Duration(timeout)*time.Second)
+	if err != nil {
 		Debug.Println("not connected")
-		return 0
+		return false
 	}
+	conn.Close()
 
-}
-
-func TCPCheckForever(IP string, Port int, interval int) int {
-	endpoint := fmt.Sprintf("%s:%v", IP, Port)
-	//fmt.Println(endpoint)
-	for {
-		one := make([]byte, 1)
-		time.Sleep(1 * time.Second)
-		conn, err := net.DialTimeout("tcp", endpoint, 1*time.Second)
-		// t := time.Now()
-		if err == nil {
-			defer conn.Close()
-			//fmt.Fprint(conn, "GET / HTTP/1.1\r\nHOST:localhost:8001\r\n\r\n")
-			conn.SetReadDeadline(time.Now().Add(1 * time.Second))
-			//data, err := ioutil.ReadAll(conn)
-			num, err := conn.Read(one)
-			if err == io.EOF {
-				return num
-			}
-			return num
-			//fmt.Printf("%s", data)
-		} else {
-			return 0
-			//log.Fatal("connection err")
-		}
-
-		//
-		//status, _ := bufio.NewReader(conn).ReadString('\n')
-		//buf := make([]byte, 1000000)
-		//var n = 1
-		//for n >= 1 {
-		//n, _ = conn.Read(buf)
-		//r, size := utf8.DecodeRune(buf)
-		//fmt.Printf(string(buf))
-		//}
-
-		//when calling log.Fatel, os.Exit(1) happend
-
-	}
-
+	return true
 }
